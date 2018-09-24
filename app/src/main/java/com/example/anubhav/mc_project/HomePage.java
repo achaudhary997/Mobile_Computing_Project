dde8cefbd095cb36bcc1fa5186ce45adf0e05a61
Anubhav Chaudhary <anubhav16013@iiitd.ac.in>
2018-09-24 22:45:08 +0530

    Updated id in xml.
    Updated id in xml for various menu items.

--- app/src/main/java/com/example/anubhav/mc_project/HomePage.java
+++ app/src/main/java/com/example/anubhav/mc_project/HomePage.java
@@ -80,17 +80,19 @@ public class HomePage extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_home) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_my_events) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_explore) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_profile) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_settings) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_my_teams) {
+
+        } else if (id == R.id.nav_logout) {
 
         }
 
