da65af9bc2964cace245edafdbe0e6583f719176
dvatsav <deepak.srivatsav@gmail.com>
2018-10-02 11:51:29 +0530

    added firebase auth to login for emails

--- app/src/main/java/com/example/anubhav/mc_project/Login.java
+++ app/src/main/java/com/example/anubhav/mc_project/Login.java
@@ -1,23 +1,38 @@
 package com.example.anubhav.mc_project;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class Login extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+
+public class Login extends AppCompatActivity{
 
     private Button loginButton;
     private EditText loginEmail, loginPassword;
     private TextView registerButton;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         initializeElements();
+
+        mAuth = FirebaseAuth.getInstance();
+
+
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -30,6 +45,14 @@ public class Login extends AppCompatActivity {
                 register();
             }
         });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
     }
 
     private void initializeElements() {
@@ -40,20 +63,27 @@ public class Login extends AppCompatActivity {
     }
 
     private void login() {
-        String email = loginEmail.getText().toString();
-        String password = loginPassword.getText().toString();
-        if (validate(email, password)) {
-            ;
-        } else {
-            ;
-        }
+        validate(loginEmail.getText().toString(), loginPassword.getText().toString());
     }
 
     private void register() {
         ;
     }
 
-    private boolean validate(String email, String password) {
-        return true;
+    private void validate(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            System.out.println("Login Succsessful");
+                            //start main activity
+                        } else {
+                            Toast.makeText(Login.this, "Authentication Failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
+
+
 }
